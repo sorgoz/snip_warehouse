0734aae175ae6b58ad93a663f02863d077903f5b
Sergey <sorgoz@users.noreply.github.com>
2019-10-23 21:45:27 +0300

    Changed order of chromosomes and replaced the source file names

--- run.py
+++ run.py
@@ -9,10 +9,10 @@ input("Are you sure you want to re-init DB?"
       "This takes 2-3 hrs per chromosome")
 init_db(DB_NAME)
 snip_loader = SnipLoader(DB_NAME)
-chr_suffixes = [i for i in range(1, 23)]
-chr_suffixes += ["X", "Y", "MT"]
+chr_suffixes = [ "Y", "MT", "X"]
+chr_suffixes += [i for i in range(1, 23)]
 for chr_suffix in chr_suffixes:
-    snip_loader.download_dbsnp_file(f"refsnp-chr{chr_suffix}.json.gz",
+    snip_loader.download_dbsnp_file(f"refsnp-chr{chr_suffix}.json.bz2",
                                     chr_suffix)
     snip_loader.load_ref_snps(
         f"refsnp-chr{chr_suffix}.json.gz", str(chr_suffix))
